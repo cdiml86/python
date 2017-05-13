fc424aa9e1324792f8bf533fe5d0df5810ab89e0
陈丹 <charles@chendandeMacBook-Air.local>
2017-05-14 01:16:51 +0800

    First Commit

---
+++ Study/imooc/py.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Created by Charles
+a = 100 +200
+print (a)
+
+l = [933,3003,30]
+l.insert(1,a)
+print (l)
+
+import os
+dangqianlujing = os.getcwd()
+print ('dangqianlujing = ',dangqianlujing)
+
+print ('你好')
