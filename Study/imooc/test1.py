607d6dd295f981de9aea1fb74bb272b0e7d1c2d3
cdiml86 <chendan@chendan.vip>
2017-07-03 21:41:31 +0800

    20170703

---
+++ Study/imooc/test1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+# Created by Charles
+import time
+import os
+while 1:
+	print('test1')
+	os.system('python3 test.py')
+	time.sleep(10)
---
+++ Study/oldboy/day2/day2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by Charles  www.chendan.wang
+
+import 
+
+f = open('disable_user.rtf','r')
+lines = f.readlines()
+
+for l in lines:
+    print()
